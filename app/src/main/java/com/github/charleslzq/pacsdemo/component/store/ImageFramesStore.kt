6284d0c54261cda34aa483415f150d36d77929cb
Eric Charles <github@ericcharleslzq.com>
2017-12-19 11:24:57 +0800

    fix bug on measure

--- app/src/main/java/com/github/charleslzq/pacsdemo/component/store/ImageFramesStore.kt
+++ app/src/main/java/com/github/charleslzq/pacsdemo/component/store/ImageFramesStore.kt
@@ -150,6 +150,7 @@ class ImageFramesStore(val layoutPosition: Int) : WithReducer<ImageFramesStore> 
             on<ClickEvent.TurnToMeasureLine>(precondition = { targetAtThis(it) }) { Measure.LINE }
             on<ClickEvent.TurnToMeasureAngle>(precondition = { targetAtThis(it) }) { Measure.ANGEL }
             on<ImageDisplayEvent.MeasureModeReset>(precondition = { targetAtThis(it) }) { Measure.NONE }
+            on<ImageDisplayEvent.IndexChange>(precondition = {targetAtThis(it)}) { Measure.NONE }
         }
 
         reduce(ImageFramesStore::imageCanvasModel) {
@@ -166,9 +167,8 @@ class ImageFramesStore(val layoutPosition: Int) : WithReducer<ImageFramesStore> 
                         state.texts.toMutableMap().apply { put(event.text.first, event.text.second) }
                 )
             }
-            on<ImageDisplayEvent.MeasureModeReset>(precondition = { targetAtThis(it) }) {
-                ImageCanvasModel()
-            }
+            on<ImageDisplayEvent.MeasureModeReset>(precondition = { targetAtThis(it) }) { ImageCanvasModel() }
+            on<ImageDisplayEvent.IndexChange>(precondition = {targetAtThis(it)}) { ImageCanvasModel() }
         }
 
         reduce(property = ImageFramesStore::currentPath) {
