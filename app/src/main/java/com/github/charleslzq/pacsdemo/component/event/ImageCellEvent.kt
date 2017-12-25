4b5986267554c0fe38ed0a119d148485de21b66e
Eric Charles <github@ericcharleslzq.com>
2017-12-25 16:34:49 +0800

    change event class structure

--- app/src/main/java/com/github/charleslzq/pacsdemo/component/event/ImageCellEvent.kt
+++ app/src/main/java/com/github/charleslzq/pacsdemo/component/event/ImageCellEvent.kt
@@ -3,4 +3,6 @@ package com.github.charleslzq.pacsdemo.component.event
 /**
  * Created by charleslzq on 17-12-11.
  */
-abstract class ImageCellEvent(val layoutPosition: Int)
+abstract class ImageCellEvent {
+    abstract val layoutPosition: Int
+}
