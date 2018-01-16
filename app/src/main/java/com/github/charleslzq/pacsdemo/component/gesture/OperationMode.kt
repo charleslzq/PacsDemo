1a5b8ca3e8429db3f53e3205aeba496ed7e5cdc7
Eric Charles <github@ericcharleslzq.com>
2018-01-16 10:58:11 +0800

    rewrite gesture mode system

--- app/src/main/java/com/github/charleslzq/pacsdemo/component/gesture/OperationMode.kt
+++ app/src/main/java/com/github/charleslzq/pacsdemo/component/gesture/OperationMode.kt
@@ -9,29 +9,37 @@ import android.view.View
 /**
  * Created by charleslzq on 17-11-24.
  */
-sealed class OperationMode(
-        private val gestureDetector: GestureDetector,
-        private val scaleGestureDetector: ScaleGestureDetector,
-        private val additionalHandler: (View, MotionEvent) -> Boolean
-) : View.OnTouchListener {
-
-    override fun onTouch(view: View, motionEvent: MotionEvent): Boolean {
-        view.performClick()
-        return additionalHandler(view, motionEvent) || gestureDetector.onTouchEvent(motionEvent) || scaleGestureDetector.onTouchEvent(motionEvent)
-    }
+sealed class OperationMode(private vararg val listeners: (View, MotionEvent) -> Boolean) : View.OnTouchListener {
+    override fun onTouch(view: View, motionEvent: MotionEvent) = listeners.any { it(view, motionEvent) }
 }
 
 class PlayMode(
         context: Context,
-        compositeGestureListener: NoOpCompositeGestureListener
-) : OperationMode(GestureDetector(context, compositeGestureListener), ScaleGestureDetector(context, compositeGestureListener), compositeGestureListener::onOtherGesture)
+        playModeGestureListener: PlayModeGestureListener,
+        private val gestureDetector: GestureDetector = GestureDetector(context, playModeGestureListener),
+        private val scaleGestureDetector: ScaleGestureDetector = ScaleGestureDetector(context, playModeGestureListener))
+    : OperationMode(
+        { _, motionEvent -> scaleGestureDetector.onTouchEvent(motionEvent) },
+        { _, motionEvent -> gestureDetector.onTouchEvent(motionEvent) }
+)
 
 class StudyMode(
         context: Context,
-        compositeGestureListener: NoOpCompositeGestureListener
-) : OperationMode(GestureDetector(context, compositeGestureListener), ScaleGestureDetector(context, compositeGestureListener), compositeGestureListener::onOtherGesture)
+        studyModeGestureListener: StudyModeGestureListener,
+        private val gestureDetector: GestureDetector = GestureDetector(context, studyModeGestureListener),
+        private val scaleGestureDetector: ScaleGestureDetector = ScaleGestureDetector(context, studyModeGestureListener))
+    : OperationMode(
+        { _, motionEvent -> scaleGestureDetector.onTouchEvent(motionEvent) },
+        { _, motionEvent -> gestureDetector.onTouchEvent(motionEvent) }
+)
 
 class MeasureMode(
         context: Context,
-        compositeGestureListener: NoOpCompositeGestureListener
-) : OperationMode(GestureDetector(context, compositeGestureListener), ScaleGestureDetector(context, compositeGestureListener), compositeGestureListener::onOtherGesture)
+        measureModeGestureListener: MeasureModeGestureListener,
+        private val gestureDetector: GestureDetector = GestureDetector(context, measureModeGestureListener),
+        private val scaleGestureDetector: ScaleGestureDetector = ScaleGestureDetector(context, measureModeGestureListener))
+    : OperationMode(
+        measureModeGestureListener::onOtherGesture,
+        { _, motionEvent -> scaleGestureDetector.onTouchEvent(motionEvent) },
+        { _, motionEvent -> gestureDetector.onTouchEvent(motionEvent) }
+)
