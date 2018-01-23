801867d3fb4fc8cf4038bdb60ad88e8786545de1
Eric Charles <github@ericcharleslzq.com>
2018-01-23 15:47:45 +0800

    remove scale functionality

--- app/src/main/java/com/github/charleslzq/pacsdemo/component/gesture/ScaleCompositeGestureListener.kt
+++ app/src/main/java/com/github/charleslzq/pacsdemo/component/gesture/ScaleCompositeGestureListener.kt
@@ -1,10 +1,7 @@
 package com.github.charleslzq.pacsdemo.component.gesture
 
-import android.graphics.PointF
 import android.view.MotionEvent
-import android.view.ScaleGestureDetector
 import com.github.charleslzq.pacsdemo.component.store.ImageFrameStore.ImageClicked
-import com.github.charleslzq.pacsdemo.component.store.ImageFrameStore.ScaleChange
 
 /**
  * Created by charleslzq on 17-11-29.
@@ -14,15 +11,6 @@ open class ScaleCompositeGestureListener(val dispatch: (Any) -> Unit) :
 
     override fun onSingleTapConfirmed(motionEvent: MotionEvent): Boolean {
         dispatch(ImageClicked())
-        return true
-    }
-
-    override fun onScaleBegin(scaleGestureDetector: ScaleGestureDetector) = true
-
-    override fun onScale(scaleGestureDetector: ScaleGestureDetector): Boolean {
-        scaleGestureDetector.run {
-            dispatch(ScaleChange(scaleFactor, PointF(focusX, focusY)))
-        }
         return true
     }
 }
